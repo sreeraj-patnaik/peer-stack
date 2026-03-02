bebfd9b4b0a36e900559b1121f0232eac9fe3e58
Sachin241 <sachinkpsk24@gmail.com>
2026-03-02 21:48:04 +0530

    completed usermodule

--- peerstack/users/models.py
+++ peerstack/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    bio = models.TextField(blank=True)
+    github_profile = models.URLField(blank=True)
+    linkedin_profile = models.URLField(blank=True)
+
+    year = models.IntegerField(blank=True, null=True)
+    college = models.CharField(max_length=255, blank=True)
+    dept = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.user.username
